c29c9ea09775678241cdda654a3c806db3933902
PenPen <59729220+JulienRaynal@users.noreply.github.com>
2023-02-27 18:04:54 +0100

    Commented the file

--- zcat.sh
+++ zcat.sh
@@ -1,11 +1,17 @@
+# For each folder in our current folder
 for GROUP in */;
-    do NEWDIR=$(echo test$GROUP | tr -d /);
+    # Create a new folder with same name prefixed with data for each detected folder
+    do NEWDIR=$(echo data$GROUP | tr -d /);
     mkdir $NEWDIR;
+    # For each barcode folder (containing 1 or multiple barcodes) 
     for BARCODE in ${GROUP}*pass/*/;  
         do 
+        # Computes a filename using the barcode
         FILENAME=$(echo $BARCODE | cut -d '/' -f1,3 | tr / -).fq.stats;
+        # For each file in the barcode folder
         for FILES in ${BARCODE}*.fastq.gz;
-           do zcat $FILES >> $NEWDIR/$FILENAME; 
+            # Concatenates the barcode files content in a file (1 file per barcode)
+            do zcat $FILES >> $NEWDIR/$FILENAME; 
         done; 
     echo ==================== $BARCODE ================;  
     done; 
