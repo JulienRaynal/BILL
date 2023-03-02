fbb97f8e0a6536349167bdb204fd34a1041f04ee
PenPen <59729220+JulienRaynal@users.noreply.github.com>
2023-03-02 15:32:02 +0100

    Commented data_grepper.sh

--- data_grepper.sh
+++ data_grepper.sh
@@ -1,13 +1,16 @@
 $(echo "" > oscur.txt)
 FILENAMES=()
 COUNTER=0
+# For each VCF file in the folder: save the VCF file name
 for FILE in ./*.vcf; do 
 	FILENAMES[${#FILENAMES[@]}]=$FILE;
 done;
 IFS=$'\n' SORTED=($(sort <<<"${FILENAMES[*]}")); unset IFS;
 
+# For each VCF file name
 for ((idx=0; idx<=${#SORTED[@]} - 1; ++idx)); do
 	echo "$idx" "${SORTED[idx]}"
+	# Get the data in the file that we are interested in
 	ID="$(cat ${SORTED[idx]}  | grep -v "^#" | cut -d$'\t'  -f2)"
 	REF=$(cat ${SORTED[idx]}  | grep -v "^#" | cut -d$'\t'  -f4)
 	ALT=$(cat ${SORTED[idx]}  | grep -v "^#" | cut -d$'\t'  -f5)
@@ -19,10 +22,11 @@ for ((idx=0; idx<=${#SORTED[@]} - 1; ++idx)); do
 	DR=$(grep -v "^#" ${SORTED[idx]} | cut -d$'\t' -f10 | cut -d ':' -f3) 
 	DV=$(grep -v "^#" ${SORTED[idx]} | cut -d$'\t' -f10 | cut -d ':' -f4) 
  
-
+	# Get the passages and the cultures
 	PASSAGE=$(echo ${SORTED[idx]} | grep -oE P[0-9].)
 	CULTURE=$(echo ${SORTED[idx]} | grep -oE C[0-9]*)
 
+	# Append to a file all the data we have saved
 	FILE=$(paste <(printf %s "$ID") <(printf %s "$REF") <(printf %s "$ALT") <(printf %s "$SVTYPE") <(printf %s "$SVLEN") <(printf %s "$END") <(printf %s "$AF") <(printf %s "$PASSAGE") <(printf %s "$CULTURE") <(printf %s "$DR") <(printf %s "$DV"))
 	$(echo "$FILE" >> oscur.txt)
 	
